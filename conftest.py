c780f285a403f3c30f4ab3f8c33066de786b7e73
testqasrt <ayakhlakov@srt-llc.ru>
2024-08-28 18:34:10 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function')
 def driver():
-    _driver = webdriver.Chrome()
+    _driver = webdriver.Firefox()
     _driver.maximize_window()
     yield _driver
     _driver.quit()
